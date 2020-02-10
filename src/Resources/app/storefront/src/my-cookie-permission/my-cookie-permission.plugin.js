579bfc142b2df89fa3ac365ffc668362e527e393
Christopher Dosin <hello@christopherdosin.me>
2020-02-10 08:29:06 +0200

    Fixed wrong path

--- src/Resources/app/storefront/src/my-cookie-permission/my-cookie-permission.plugin.js
+++ src/Resources/app/storefront/src/my-cookie-permission/my-cookie-permission.plugin.js
@@ -1,4 +1,4 @@
-import CookiePermissionPlugin from 'src/plugin/cookie-permission/cookie-permission.plugin';
+import CookiePermissionPlugin from 'src/plugin/cookie/cookie-permission.plugin';
 import CookieStorage from 'src/helper/storage/cookie-storage.helper';
 
 export default class MyCookiePermission extends CookiePermissionPlugin {
